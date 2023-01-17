7934b28eef0c67763226e504a3bec8d78b98b015
ptchblvck <ptchblvck@icloud.com>
2023-01-18 00:19:58 +0100

    added a button to reforge until next extract

--- MultiReforging.lua
+++ MultiReforging.lua
@@ -2,7 +2,9 @@
 
 local CollectionsFrame = _G["Collections"] -- The Collections Frame with talents, mystic enchants, hero architect, etc.
 local mysticRuneId = 98462
+local mysticExtractId = 98463
 local mysticRuneCount = GetItemCount(mysticRuneId) -- counts the current amount of Mystic Runes
+local mysticExtractCount = GetItemCount(mysticExtractId) -- counts the current amount of Mystic Extracts
 local mysticEnchantReforgeOnceButton = _G["MysticEnchantingFrameControlFrameRollButton"] -- the reforge button from the main UI
 local mysticEnchantingFameToken = _G["MysticEnchantingFrameControlFrameTokenFrameTokenButton"] -- the check token for enabling mysticEnchantReforgeTenTimes and mysticEnchantReforgeAll
 
@@ -21,6 +23,7 @@ end)
 
 -- checks if the player casts anything or not. currently not in use in this addon
 
+--[[
 local playerCasts = false
 if UnitCastingInfo("player") or UnitChannelInfo("player") then
     -- player is casting or channeling a spell
@@ -29,11 +32,12 @@ else
     -- player is not casting or channeling a spell
     local playerCasts = false
 end
+]]--
 
 -- creating the frame for the additional reforging options
 
 local frame = CreateFrame("Frame", "MultiReforgingFrame", UIParent)
-frame:SetSize(220, 80)
+frame:SetSize(220, 120)
 frame:SetPoint("TOPRIGHT", CollectionsFrame, "TOPRIGHT", 205, -5)
 
 local frameHeading = CreateFrame("Frame", "MultiReforgingHeadingFrame", frame)
@@ -89,14 +93,14 @@ SlashCmdList["SHOW MULTI"] = ShowMultiReforgingFrame
 
 local mysticEnchantReforgeTenTimes = CreateFrame("Button", "ReforgeAction", frame, "UIPanelButtonTemplate")
 mysticEnchantReforgeTenTimes:SetSize(120, 25)
-mysticEnchantReforgeTenTimes:SetPoint("TOP", frame, "TOP", 0, -15)
+mysticEnchantReforgeTenTimes:SetPoint("TOP", frame, "TOP", 0, -25)
 mysticEnchantReforgeTenTimes:SetText("Reforge 10x")
 mysticEnchantReforgeTenTimes:SetEnabled(false)
 
 mysticEnchantReforgeTenTimes:SetScript("OnEnter", function(self)
     GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
     GameTooltip:SetText("Reforge 10x")
-    GameTooltip:AddLine("Click this button to reforge 10 times", 1, 1, 1)
+    GameTooltip:AddLine("Click this button to reforge |cffA600FF10 |cffFFFFFFtimes", 1, 1, 1)
     GameTooltip:Show()
 end)
 
@@ -104,11 +108,30 @@ mysticEnchantReforgeTenTimes:SetScript("OnLeave", function(self)
     GameTooltip:Hide()
 end)
 
+-- creates a button that will reforge until receiving another mystic extract
+
+local mysticExtractReforge = CreateFrame("Button", "ReforgeExtract", frame, "UIPanelButtonTemplate")
+mysticExtractReforge:SetSize(170, 25)
+mysticExtractReforge:SetPoint("TOP", mysticEnchantReforgeTenTimes, "TOP", 0, -25)
+mysticExtractReforge:SetText("Reforge until Extract")
+mysticExtractReforge:SetEnabled(false)
+
+mysticExtractReforge:SetScript("OnEnter", function(self)
+    GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+    GameTooltip:SetText("Reforge Mystic Runes")
+    GameTooltip:AddLine("|cffFFFFFFuntil receiving another |cffA600FFMystic Extract", 1, 1, 1)
+    GameTooltip:Show()
+end)
+
+mysticExtractReforge:SetScript("OnLeave", function(self)
+    GameTooltip:Hide()
+end)
+
 -- creates a button that will be the "reforgeall" button to reforge for all mystic runes you have and stop when you have 0 left
 
 local mysticEnchantReforgeAll = CreateFrame("Button", "ReforgeAll", frame, "UIPanelButtonTemplate")
 mysticEnchantReforgeAll:SetSize(160, 25)
-mysticEnchantReforgeAll:SetPoint("TOP", mysticEnchantReforgeTenTimes, "TOP", 0, -25)
+mysticEnchantReforgeAll:SetPoint("TOP", mysticExtractReforge, "TOP", 0, -25)
 mysticEnchantReforgeAll:SetText("Reforge All (" .. mysticRuneCount .. ")")
 mysticEnchantReforgeAll:SetEnabled(false)
 
@@ -171,6 +194,7 @@ end)
 
 local function OnUpdate(self, elapsed)
     local mysticRuneCount = GetItemCount(mysticRuneId)
+    local mysticExtractCount = GetItemCount(mysticExtractId)
     if frame:IsShown() then
         mysticEnchantReforgeAll:SetText("Reforge All (" .. mysticRuneCount .. ")")
     end    
@@ -185,6 +209,7 @@ frame:RegisterEvent("PLAYER_LOGIN")
 local function OnEvent(self, event, ...)
     if event == "PLAYER_LOGIN" then
         local mysticRuneCount = GetItemCount(mysticRuneId)
+        local mysticExtractCount = GetItemCount(mysticExtractId)
         print("|cffFE67D2MultiReforging v0.0.12 beta by " .. "|cff67FE93ptchblvck" .. "|cffFE67D2 loaded")
         C_Timer.After(5, function() -- wait 5 seconds before checking
             if CollectionsFrame:IsShown() and mysticEnchantReforgeOnceButton:IsShown() then
@@ -248,16 +273,47 @@ end
 
 mysticEnchantReforgeAll:SetScript("OnClick", OnClickAllRunes)
 
+-- this is the main callback function for reforging until you get another mystic extract
+
+local currentExtracts = mysticExtractCount
+
+local function OnClickExtract()
+    if mysticEnchantReforgeOnceButton:IsShown() then
+        -- Code to click the other button
+        mysticEnchantReforgeOnceButton:Click()
+        WaitForAction(function() return not mysticEnchantReforgeOnceButton:IsEnabled() end, 3)
+        clicks = clicks + 1
+        reforgeCount = reforgeCount + 1
+        local mysticRuneCount = GetItemCount(mysticRuneId)
+        local mysticExtractCount = GetItemCount(mysticExtractId)
+        if mysticExtractCount == currentExtracts and clicks < mysticRuneCount then
+            C_Timer.After(1.1, function() mysticExtractReforge:Click() end)
+        else
+            clicks = 0
+            C_Timer.After(1.1, function() print("|cff00FFFFReforged " .. "|cffA600FF" .. reforgeCount .. "|cff00FFFF times")
+            print("|cff00FFFFReceived 1 additional |cffA600FFMystic Extract") end)
+        end
+        -- print("|cffFA9BFFMystic Rune count: " .. "|cffA600FF" .. mysticRuneCount) -- this is not needed for now
+    else
+        mysticExtractReforge:SetEnabled(false)
+        print("|cffFF0000Button is disabled, action can not be performed.")
+    end
+end
+
+mysticExtractReforge:SetScript("OnClick", OnClickExtract)
+
 -- if the token is shown sets buttons to enabled and also updates the mysticRuneCount variable
 
 mysticEnchantingFameToken:HookScript("OnShow", function()
     local mysticRuneCount = GetItemCount(mysticRuneId)
     if mysticEnchantReforgeOnceButton:IsEnabled() then
         mysticEnchantReforgeTenTimes:SetEnabled(true)
+        mysticExtractReforge:SetEnabled(true)
         mysticEnchantReforgeAll:SetEnabled(true)
     end
     if mysticRuneCount < 1 then
         mysticEnchantReforgeTenTimes:SetEnabled(false)
+        mysticExtractReforge:SetEnabled(false)
         mysticEnchantReforgeAll:SetEnabled(false)
     end
 end)
@@ -265,6 +321,7 @@ end)
 mysticEnchantingFameToken:HookScript("OnHide", function()
     local mysticRuneCount = GetItemCount(mysticRuneId)
         mysticEnchantReforgeTenTimes:SetEnabled(false)
+        mysticExtractReforge:SetEnabled(false)
         mysticEnchantReforgeAll:SetEnabled(false)
 end)
 
